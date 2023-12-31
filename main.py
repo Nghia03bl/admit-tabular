e8982b22f775f8c146769475523ef6dc5961277e
Nghia03bl <117901265+Nghia03bl@users.noreply.github.com>
2023-12-31 18:49:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,36 @@ from PIL import Image
 import pickle as pkl
 import numpy as np
 
-st.title('USA college admission rate prediction')
+# Set page title
+st.title('USA College Admission Rate Prediction')
 
-image = Image.open('college admission.jpeg')
-st.image(image)
+# Load and display an image
+image = Image.open('college_admission.jpeg')
+st.image(image, caption='College Admission', use_column_width=True)
 
+# Load the pre-trained model
 input = open('lr_admit.pkl', 'rb')
 model = pkl.load(input)
 
-st.header('Input admission information')
+# Display header for input admission information
+st.header('Input Admission Information')
+
+# Input form for user
 gre = st.number_input('Insert GRE Score')
 toefl = st.number_input('Insert TOEFL Score')
 uni_rate = st.number_input('Insert University Rating')
 sop = st.number_input('Insert SOP')
 lor = st.number_input('Insert LOR')
 cgpa = st.number_input('Insert CGPA')
-research = st.radio('Choose Research', [0, 1], index=None)
+research = st.radio('Choose Research Experience', [0, 1], index=None)
 
-if gre is not None and toefl is not None and uni_rate is not None and sop is not None and lor is not None and cgpa is not None and research is not None:
+# Check if all input fields are filled
+if all([gre, toefl, uni_rate, sop, lor, cgpa, research]):
+    # Make prediction when the 'Predict' button is clicked
     if st.button('Predict'):
-        feature_vector = np.array([gre, toefl, uni_rate, sop, lor, cgpa, research]).reshape(1,-1)
-        result = str((model.predict(feature_vector)[0])[0])
+        feature_vector = np.array([gre, toefl, uni_rate, sop, lor, cgpa, research]).reshape(1, -1)
+        result = model.predict(feature_vector)[0]
 
+        # Display the result
         st.header('Result')
-        st.text(result)
+        st.markdown(f'The predicted admission rate is: **{result:.2%}**')
